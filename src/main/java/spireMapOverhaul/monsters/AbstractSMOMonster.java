57fc3d2f5f4568ee4f8884b2a7f6b5e9201af733
erasels <emanuelbedburdick@yahoo.de>
2023-11-30 21:25:38 +0100

    Add monster abstract from BattleTowers

---
+++ src/main/java/spireMapOverhaul/monsters/AbstractSMOMonster.java
@@ -0,0 +1,130 @@
+package spireMapOverhaul.monsters;
+
+import basemod.abstracts.CustomMonster;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.EnemyMoveInfo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractSMOMonster extends CustomMonster {
+    protected Map<Byte, EnemyMoveInfo> moves;
+    protected boolean firstMove = true;
+    private static final float ASCENSION_DAMAGE_BUFF_PERCENT = 1.10f;
+    private static final float ASCENSION_TANK_BUFF_PERCENT = 1.10f;
+    private static final float ASCENSION_SPECIAL_BUFF_PERCENT = 1.5f;
+
+    public AbstractSMOMonster(String name, String id, int maxHealth, float hb_x, float hb_y, float hb_w, float hb_h, String imgUrl, float offsetX, float offsetY) {
+        super(name, id, maxHealth, hb_x, hb_y, hb_w, hb_h, imgUrl, offsetX, offsetY);
+        setUpMisc();
+    }
+
+    public AbstractSMOMonster(String name, String id, int maxHealth, float hb_x, float hb_y, float hb_w, float hb_h, String imgUrl, float offsetX, float offsetY, boolean ignoreBlights) {
+        super(name, id, maxHealth, hb_x, hb_y, hb_w, hb_h, imgUrl, offsetX, offsetY, ignoreBlights);
+        setUpMisc();
+    }
+
+    public AbstractSMOMonster(String name, String id, int maxHealth, float hb_x, float hb_y, float hb_w, float hb_h, String imgUrl) {
+        super(name, id, maxHealth, hb_x, hb_y, hb_w, hb_h, imgUrl);
+        setUpMisc();
+    }
+
+    protected void setUpMisc() {
+        moves = new HashMap<>();
+        this.dialogX = (this.hb_x - 70.0F) * Settings.scale;
+        this.dialogY -= (this.hb_y - 55.0F) * Settings.scale;
+    }
+
+    protected void addMove(byte moveCode, Intent intent) {
+        this.addMove(moveCode, intent, -1);
+    }
+    protected void addMove(byte moveCode, Intent intent, int baseDamage) {
+        this.addMove(moveCode, intent, baseDamage, 0, false);
+    }
+    protected void addMove(byte moveCode, Intent intent, int baseDamage, int multiplier) {
+        this.addMove(moveCode, intent, baseDamage, multiplier, multiplier > 0);
+    }
+    protected void addMove(byte moveCode, Intent intent, int baseDamage, int multiplier, boolean isMultiDamage) {
+        this.moves.put(moveCode, new EnemyMoveInfo(moveCode, intent, baseDamage, multiplier, isMultiDamage));
+    }
+
+    public void setMoveShortcut(byte next, String text) {
+        EnemyMoveInfo info = this.moves.get(next);
+        this.setMove(text, next, info.intent, info.baseDamage, info.multiplier, info.isMultiDamage);
+    }
+    public void setMoveShortcut(byte next) {
+        this.setMoveShortcut(next, null);
+    }
+
+    protected int calcAscensionDamage(float base) {
+        switch (this.type) {
+            case BOSS:
+                if(AbstractDungeon.ascensionLevel >= 4) {
+                    base *= ASCENSION_DAMAGE_BUFF_PERCENT;
+                }
+                break;
+            case ELITE:
+                if(AbstractDungeon.ascensionLevel >= 3) {
+                    base *= ASCENSION_DAMAGE_BUFF_PERCENT;
+                }
+                break;
+            case NORMAL:
+                if(AbstractDungeon.ascensionLevel >= 2) {
+                    base *= ASCENSION_DAMAGE_BUFF_PERCENT;
+                }
+                break;
+        }
+        return Math.round(base);
+    }
+
+    protected int calcAscensionTankiness(float base) {
+        switch (this.type) {
+            case BOSS:
+                if (AbstractDungeon.ascensionLevel >= 9) {
+                    base *= ASCENSION_TANK_BUFF_PERCENT;
+                }
+                break;
+            case ELITE:
+                if (AbstractDungeon.ascensionLevel >= 8) {
+                    base *= ASCENSION_TANK_BUFF_PERCENT;
+                }
+                break;
+            case NORMAL:
+                if (AbstractDungeon.ascensionLevel >= 7) {
+                    base *= ASCENSION_TANK_BUFF_PERCENT;
+                }
+                break;
+        }
+        return Math.round(base);
+    }
+
+    protected int calcAscensionSpecial(float base) {
+        switch (this.type) {
+            case BOSS:
+                if(AbstractDungeon.ascensionLevel >= 19) {
+                    base *= ASCENSION_SPECIAL_BUFF_PERCENT;
+                }
+                break;
+            case ELITE:
+                if(AbstractDungeon.ascensionLevel >= 18) {
+                    base *= ASCENSION_SPECIAL_BUFF_PERCENT;
+                }
+                break;
+            case NORMAL:
+                if(AbstractDungeon.ascensionLevel >= 17) {
+                    base *= ASCENSION_SPECIAL_BUFF_PERCENT;
+                }
+                break;
+        }
+        return Math.round(base);
+    }
+
+    @Override
+    public void die(boolean triggerRelics) {
+        // Makes the monster shake when it dies
+        this.useShakeAnimation(5.0F);
+        super.die(triggerRelics);
+    }
+
+}
