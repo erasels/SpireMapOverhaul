22eb89e2e9b609b8f1cc480732388fd80b290a42
erasels <emanuelbedburdick@yahoo.de>
2023-12-17 15:18:48 +0100

    Add base cost modification to ShopModifiyingZone

--- src/main/java/spireMapOverhaul/patches/interfacePatches/ShopModifierPatches.java
+++ src/main/java/spireMapOverhaul/patches/interfacePatches/ShopModifierPatches.java
@@ -4,6 +4,7 @@ import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.shop.Merchant;
+import com.megacrit.cardcrawl.shop.ShopScreen;
 import javassist.CannotCompileException;
 import javassist.CtBehavior;
 import spireMapOverhaul.util.Wiz;
@@ -26,6 +27,21 @@ public class ShopModifierPatches {
             public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
                 Matcher finalMatcher = new Matcher.FieldAccessMatcher(Merchant.class, "speechTimer");
                 return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+            }
+        }
+    }
+
+    @SpirePatch2(clz= ShopScreen.class, method = "initCards")
+    public static class BaseCostModificationHook {
+        @SpireInsertPatch(locator = Locator.class, localvars = {"c", "tmpPrice"})
+        public static void patch(AbstractCard c, @ByRef float[] tmpPrice) {
+            Wiz.forCurZone(ShopModifyingZone.class, z -> tmpPrice[0] = z.modifyCardBaseCost(c, tmpPrice[0]));
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+                Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractCard.class, "price");
+                return LineFinder.findAllInOrder(ctMethodToPatch, finalMatcher);
             }
         }
     }
--- src/main/java/spireMapOverhaul/zoneInterfaces/ShopModifyingZone.java
+++ src/main/java/spireMapOverhaul/zoneInterfaces/ShopModifyingZone.java
@@ -19,4 +19,14 @@ public interface ShopModifyingZone {
      * @param idleMessages The current list of messages which can be modified to your liking.
      */
     default void postAddIdleMessages(ArrayList<String> idleMessages) {}
+
+    /**
+     * Hook to modify the card's base cost before it is modified by ascensions and relics etc
+     * @param c The card you're modifying the cost of
+     * @param baseCost The current base cost (cost of rarity multiplied by 0.9-1.1 and additionally 1.2 if colorless)
+     * @return New pre discount cost will be cast to int after
+     */
+    default float modifyCardBaseCost(AbstractCard c, float baseCost) {
+        return baseCost;
+    }
 }
