c19906a6baf09be6f29267e4d28526918dcb3911
modargo <modargo@gmail.com>
2023-12-27 03:06:18 -0500

    Invasion zone: fix act number for act 2 elite fights

--- src/main/java/spireMapOverhaul/zones/invasion/InvasionZone.java
+++ src/main/java/spireMapOverhaul/zones/invasion/InvasionZone.java
@@ -217,8 +217,8 @@ public class InvasionZone extends AbstractZone implements EncounterModifyingZone
         return Arrays.asList(
             new ZoneEncounter(Hydra.ID, 1, (BaseMod.GetMonster)Hydra::new),
             new ZoneEncounter(VoidReaper.ID, 1, (BaseMod.GetMonster)VoidReaper::new),
-            new ZoneEncounter(WarGolem.ID, 1, (BaseMod.GetMonster)WarGolem::new),
-            new ZoneEncounter(ElementalPortal.ID, 1, () -> new ElementalPortal(150.0F, 0.0F))
+            new ZoneEncounter(WarGolem.ID, 2, (BaseMod.GetMonster)WarGolem::new),
+            new ZoneEncounter(ElementalPortal.ID, 2, () -> new ElementalPortal(150.0F, 0.0F))
         );
     }
 }
