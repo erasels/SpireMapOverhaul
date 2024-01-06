6721fa28a845af0a9eb2361c4fc6e51f3a81d1bd
CommieFlowers <rasmus.cedergren@hotmail.com>
2024-01-06 05:40:22 +0100

    Fixed warnings, made getCurrentZoneCharacter good, and some more

--- src/main/java/spireMapOverhaul/zones/characterinfluence/CharacterInfluenceEvent.java
+++ src/main/java/spireMapOverhaul/zones/characterinfluence/CharacterInfluenceEvent.java
@@ -8,33 +8,22 @@ import basemod.abstracts.events.phases.TextPhase;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.GenericEventDialog;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
-import com.megacrit.cardcrawl.helpers.MonsterHelper;
 import com.megacrit.cardcrawl.helpers.RelicLibrary;
 import com.megacrit.cardcrawl.localization.EventStrings;
-import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.rewards.RewardItem;
-import com.megacrit.cardcrawl.rooms.AbstractRoom;
-import com.megacrit.cardcrawl.vfx.UpgradeShineEffect;
-import com.megacrit.cardcrawl.vfx.cardManip.ShowCardBrieflyEffect;
-import spireMapOverhaul.BetterMapGenerator;
-import spireMapOverhaul.SpireAnniversary6Mod;
-import spireMapOverhaul.abstracts.AbstractZone;
-import spireMapOverhaul.patches.ZonePatches;
 
 import java.util.ArrayList;
 
 import static spireMapOverhaul.SpireAnniversary6Mod.makeID;
+import static spireMapOverhaul.zones.characterinfluence.CharacterInfluenceZone.*;
 
+@SuppressWarnings("unused")
 public class CharacterInfluenceEvent extends PhasedEvent {
     public static final String ID = makeID("CharacterVisit");
     private static final EventStrings eventStrings = CardCrawlGame.languagePack.getEventString(ID);
@@ -42,25 +31,16 @@ public class CharacterInfluenceEvent extends PhasedEvent {
     private static final String[] OPTIONS = eventStrings.OPTIONS;
     private static final String title = eventStrings.NAME;
 
-    //TODO: Check if the event spawns forever
-
     public CharacterInfluenceEvent() {
-        super(ID, title, "images/events/ShadowyFigure.png"); //TODO: Set a default image.
+        super(ID, title, "images/events/ShadowyFigure.png");
 
-        setImage(); //If it can find a mod image, it uses that instead.
-
-        /*
-        if (ZonePatches.currentZone() instanceof CharacterInfluenceZone && ZonePatches.currentZone() != null && ((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence != null) {
-            this.imageEventText.loadImage("images/ui/charSelect/" + ((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence.getPortraitImageName());
-        }
-
-         */
+        setImage(); // If it can find a mod image, it uses that instead.
 
         registerPhase("EventStart", new TextPhase(DESCRIPTIONS[0] + "a Stranger" + DESCRIPTIONS[1]) {
             @Override
             public String getBody() {
-                if (ZonePatches.currentZone() instanceof CharacterInfluenceZone && ZonePatches.currentZone() != null && ((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence != null) {
-                    return DESCRIPTIONS[0] + ((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence.title + DESCRIPTIONS[1];
+                if (getCurrentZoneCharacter() != null) {
+                    return DESCRIPTIONS[0] + getCurrentZoneCharacter().title + DESCRIPTIONS[1];
                 } else {
                     return super.getBody();
                 }
@@ -82,8 +62,8 @@ public class CharacterInfluenceEvent extends PhasedEvent {
 
             //Adds all starting relics to the reward screen
             ArrayList<String> relicStrings = new ArrayList<>();
-            if (ZonePatches.currentZone() instanceof CharacterInfluenceZone && ZonePatches.currentZone() != null && ((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence != null) {
-                relicStrings = ((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence.getStartingRelics();
+            if (getCurrentZoneCharacter() != null) {
+                relicStrings = getCurrentZoneCharacter().getStartingRelics();
             }
 
             for (String relicID : relicStrings) {
@@ -95,15 +75,17 @@ public class CharacterInfluenceEvent extends PhasedEvent {
         transitionKey("EventStart"); //starting point
     }
 
+    // Sets the Event image to a cropped version of the character portrait.
     public void setImage() {
 
         Texture portraitTexture = null;
 
-        if (ZonePatches.currentZone() instanceof CharacterInfluenceZone && ZonePatches.currentZone() != null && ((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence != null) {
-            if (Gdx.files.internal("images/ui/charSelect/" + ((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence.getPortraitImageName()).exists()) {
-                portraitTexture = ImageMaster.loadImage("images/ui/charSelect/" + ((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence.getPortraitImageName());
-            } else if (Gdx.files.internal(BaseMod.playerPortraitMap.get(((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence.chosenClass)).exists()) {
-                portraitTexture = ImageMaster.loadImage((BaseMod.playerPortraitMap.get(((CharacterInfluenceZone) ZonePatches.currentZone()).classInfluence.chosenClass)));
+        // Finds the portrait
+        if (getCurrentZoneCharacter() != null) {
+            if (Gdx.files.internal("images/ui/charSelect/" + getCurrentZoneCharacter().getPortraitImageName()).exists()) {
+                portraitTexture = ImageMaster.loadImage("images/ui/charSelect/" + getCurrentZoneCharacter().getPortraitImageName());
+            } else if (Gdx.files.internal(BaseMod.playerPortraitMap.get(getCurrentZoneCharacter().chosenClass)).exists()) {
+                portraitTexture = ImageMaster.loadImage((BaseMod.playerPortraitMap.get(getCurrentZoneCharacter().chosenClass)));
             }
         }
 
@@ -131,6 +113,8 @@ public class CharacterInfluenceEvent extends PhasedEvent {
         );
 
         Texture newTexture = new Texture(pixmap);
+
+        // Repeats what this.imageEventText.loadImage(imgUrl) does, with ReflectionHacks.
 
         if (ReflectionHacks.getPrivate(this.imageEventText, GenericEventDialog.class, "img") != null) {
             ((Texture)ReflectionHacks.getPrivate(this.imageEventText, GenericEventDialog.class, "img")).dispose();
--- src/main/java/spireMapOverhaul/zones/characterinfluence/CharacterInfluenceZone.java
+++ src/main/java/spireMapOverhaul/zones/characterinfluence/CharacterInfluenceZone.java
@@ -1,38 +1,22 @@
 package spireMapOverhaul.zones.characterinfluence;
 
-import basemod.AutoAdd;
-import basemod.BaseMod;
-import basemod.cardmods.EtherealMod;
-import basemod.helpers.CardModifierManager;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
-import com.megacrit.cardcrawl.cards.colorless.MindBlast;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.characters.Ironclad;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.events.AbstractEvent;
-import com.megacrit.cardcrawl.helpers.CardLibrary;
-import com.megacrit.cardcrawl.helpers.ModHelper;
 import com.megacrit.cardcrawl.map.MapRoomNode;
-import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.relics.Circlet;
-import com.megacrit.cardcrawl.rewards.RewardItem;
-import com.megacrit.cardcrawl.unlock.UnlockTracker;
-import spireMapOverhaul.SpireAnniversary6Mod;
 import spireMapOverhaul.abstracts.AbstractZone;
+import spireMapOverhaul.patches.ZonePatches;
 import spireMapOverhaul.zoneInterfaces.ModifiedEventRateZone;
 import spireMapOverhaul.zoneInterfaces.OnTravelZone;
 import spireMapOverhaul.zoneInterfaces.RewardModifyingZone;
 import spireMapOverhaul.zoneInterfaces.ShopModifyingZone;
-import spireMapOverhaul.zones.example.CoolExampleEvent;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-import static spireMapOverhaul.SpireAnniversary6Mod.makeID;
-
+@SuppressWarnings("unused")
 public class CharacterInfluenceZone extends AbstractZone implements RewardModifyingZone, ShopModifyingZone, OnTravelZone, ModifiedEventRateZone {
 
     public static final String ID = "CharacterInfluence";
@@ -52,6 +36,16 @@ public class CharacterInfluenceZone extends AbstractZone implements RewardModify
     public CharacterInfluenceZone(AbstractPlayer p) {
         this();
         this.classInfluence = p;
+    }
+
+    public static AbstractPlayer getCurrentZoneCharacter() {
+        AbstractZone currentZone = ZonePatches.currentZone();
+
+        if (!(currentZone instanceof CharacterInfluenceZone) ||
+                ((CharacterInfluenceZone) currentZone).classInfluence == null)
+            return null;
+
+        return ((CharacterInfluenceZone) currentZone).classInfluence;
     }
 
     @Override
@@ -106,42 +100,18 @@ public class CharacterInfluenceZone extends AbstractZone implements RewardModify
         if (commonPool == null || uncommonPool == null || rarePool == null) initPools();
         switch (rarity) {
             case UNCOMMON:
-                return type != null ? uncommonPool.getRandomCard(type, false) : uncommonPool.getRandomCard(false);
+                return type != null ? uncommonPool.getRandomCard(type, true) : uncommonPool.getRandomCard(true);
             case RARE:
-                return type != null ? rarePool.getRandomCard(type, false) : rarePool.getRandomCard(false);
+                return type != null ? rarePool.getRandomCard(type, true) : rarePool.getRandomCard(true);
             case COMMON:
             default:
-                return type != null ? commonPool.getRandomCard(type, false) : commonPool.getRandomCard(false);
+                return type != null ? commonPool.getRandomCard(type, true) : commonPool.getRandomCard(true);
         }
     }
 
-    @Override
-    public void modifyRewardCards(ArrayList<AbstractCard> rewardCards) {
+    public void replaceCards(ArrayList<AbstractCard> cardList) {
         ArrayList<AbstractCard> newCards = new ArrayList<>();
-        for (AbstractCard card : rewardCards) {
-            boolean dupe;
-            do {
-                dupe = false;
-                AbstractCard cardToAdd = getCard(card.rarity, null);
-                for (AbstractCard newCard : newCards) {
-                    if (cardToAdd.cardID.equals(newCard.cardID)) {
-                        dupe = true;
-                        break;
-                    }
-                }
-                if (!dupe) {
-                    newCards.add(cardToAdd);
-                }
-            } while (dupe);
-        }
-        rewardCards.clear();
-        rewardCards.addAll(newCards);
-    }
-
-    @Override
-    public void postCreateShopCards(ArrayList<AbstractCard> coloredCards, ArrayList<AbstractCard> colorlessCards) {
-        ArrayList<AbstractCard> newCards = new ArrayList<>();
-        for (AbstractCard card : coloredCards) {
+        for (AbstractCard card : cardList) {
             boolean dupe;
             do {
                 dupe = false;
@@ -153,12 +123,23 @@ public class CharacterInfluenceZone extends AbstractZone implements RewardModify
                     }
                 }
                 if (!dupe) {
+                    if (card.upgraded) cardToAdd.upgrade();
                     newCards.add(cardToAdd);
                 }
             } while (dupe);
         }
-        coloredCards.clear();
-        coloredCards.addAll(newCards);
+        cardList.clear();
+        cardList.addAll(newCards);
+    }
+
+    @Override
+    public void modifyRewardCards(ArrayList<AbstractCard> rewardCards) {
+        replaceCards(rewardCards);
+    }
+
+    @Override
+    public void postCreateShopCards(ArrayList<AbstractCard> coloredCards, ArrayList<AbstractCard> colorlessCards) {
+        replaceCards(coloredCards);
     }
 
     @Override
