8e9b80cf470ae5a590b82e548bdc94ebd4c8e652
tetraminus <54343976+tetraminus@users.noreply.github.com>
2024-01-10 22:05:41 -0800

    make buffs apply to mid-battle spawns

--- src/main/java/spireMapOverhaul/zones/beastslair/BeastsLairEvent.java
+++ src/main/java/spireMapOverhaul/zones/beastslair/BeastsLairEvent.java
@@ -1,13 +1,19 @@
 package spireMapOverhaul.zones.beastslair;
 
 import basemod.AutoAdd;
+import basemod.ReflectionHacks;
 import basemod.abstracts.events.PhasedEvent;
 import basemod.abstracts.events.phases.CombatPhase;
 import basemod.abstracts.events.phases.TextPhase;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.SpawnMonsterAction;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.EventStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.ArtifactPower;
+import javassist.CtBehavior;
 import spireMapOverhaul.actions.AllEnemyApplyPowerAction;
 import spireMapOverhaul.util.Wiz;
 import spireMapOverhaul.zones.beastslair.powers.FuryPower;
@@ -45,5 +51,28 @@ public class BeastsLairEvent extends PhasedEvent {
             m.currentHealth = (int) (m.currentHealth * 1.75f);
         }
         );
+    }
+
+    @SpirePatch2(clz = SpawnMonsterAction.class, method = "update")
+    public static class SpawnMonsterActionPatch {
+        @SpireInsertPatch(locator = Locator.class)
+        public static void patch(SpawnMonsterAction __instance) {
+            boolean used = ReflectionHacks.getPrivate(__instance, SpawnMonsterAction.class, "used");
+            if (!used && AbstractDungeon.getCurrRoom().event instanceof BeastsLairEvent) {
+                AbstractMonster m = ReflectionHacks.getPrivate(__instance, SpawnMonsterAction.class, "m");
+                m.maxHealth = (int) (m.maxHealth * 1.75f);
+                m.currentHealth = (int) (m.currentHealth * 1.75f);
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, null, new ArtifactPower(m, 2), 2));
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, null, new FuryPower(m, 10), 10));
+            }
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            @Override
+            public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+                Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractMonster.class, "showHealthBar");
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+            }
+        }
     }
 }
