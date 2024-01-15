908609a7aaca8c5f38f6031ae62dce79f7051bb7
Paul-Sml <59653549+Paul-Sml@users.noreply.github.com>
2024-01-15 03:26:55 -0500

    Event starts to look cool, still bugged

--- src/main/java/spireMapOverhaul/zones/hailstorm/events/AbandonedCamp.java
+++ src/main/java/spireMapOverhaul/zones/hailstorm/events/AbandonedCamp.java
@@ -1,19 +1,38 @@
 package spireMapOverhaul.zones.hailstorm.events;
 
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.events.AbstractImageEvent;
 import com.megacrit.cardcrawl.events.city.Colosseum;
+import com.megacrit.cardcrawl.events.shrines.WomanInBlue;
 import com.megacrit.cardcrawl.helpers.MonsterHelper;
+import com.megacrit.cardcrawl.helpers.PotionHelper;
+import com.megacrit.cardcrawl.helpers.RelicLibrary;
+import com.megacrit.cardcrawl.helpers.ScreenShake;
 import com.megacrit.cardcrawl.localization.EventStrings;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.relics.AbstractRelic.RelicTier;
+import com.megacrit.cardcrawl.relics.Circlet;
+import com.megacrit.cardcrawl.relics.DeadBranch;
+import com.megacrit.cardcrawl.rewards.RewardItem;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.vfx.RainingGoldEffect;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
+import jdk.nashorn.internal.ir.IfNode;
 import spireMapOverhaul.SpireAnniversary6Mod;
+import spireMapOverhaul.zones.hailstorm.cards.Freeze;
+import spireMapOverhaul.zones.hailstorm.relics.Flint;
 import spireMapOverhaul.zones.thieveshideout.ThievesHideoutZone;
 
 // We extend the Colosseum event because ProceedButton.java specifically checks if an event is an instance of this type
 // (or a few other types) in the logic for what happens when you click proceed. This is easier than a patch.
-public class AbandonedCamp extends Colosseum {
+public class AbandonedCamp extends AbstractImageEvent {
     public static final String ID = SpireAnniversary6Mod.makeID(AbandonedCamp.class.getSimpleName());
     private static final EventStrings eventStrings = CardCrawlGame.languagePack.getEventString(ID);
     private static final String NAME = eventStrings.NAME;
@@ -21,127 +40,112 @@ public class AbandonedCamp extends Colosseum {
     private static final String[] OPTIONS = eventStrings.OPTIONS;
     public static final String IMG = SpireAnniversary6Mod.makeImagePath("events/Invasion/ThiefKing.png");
 
-    private static final int FLINT_HP_LOSS = 1/24;
-    private static final int A_15_FLINT_HP_LOSS = 1/16;
+    private static final float FLINT_HP_LOSS = (float) 1 /24;
+    private static final float A_15_FLINT_HP_LOSS = (float) 1 /16;
     private static final int GOLD = 90;
     private static final int A_15_GOLD = 60;
     private static final int HEALING = 1/4;
     private static final int A_15_HEALING = 1/6;
 
-    private CurScreen screen;
+    private boolean T_DEADBRANCH_F_FLINT;
+    private boolean T_GOLD_F_HEALING;
+
+    private AbstractCard curse = new Freeze();
+    private int hpLoss;
+    private int goldGain;
+    private int healing;
+
 
     public AbandonedCamp() {
-        super();
-        this.imageEventText.clear();
-        this.roomEventText.clear();
-        this.title = NAME;
-        this.body = DESCRIPTIONS[0];
-        this.imageEventText.loadImage(IMG);
-        type = EventType.IMAGE;
-        this.noCardsInRewards = false;
+        super(ID, NAME ,IMG );
+        this.noCardsInRewards = true;
 
-        this.screen = CurScreen.INTRO;
+        //Event randomly chooses one big reward between two, and one light reward between two
+        T_DEADBRANCH_F_FLINT = AbstractDungeon.miscRng.randomBoolean();
+        T_GOLD_F_HEALING = AbstractDungeon.miscRng.randomBoolean();
 
+        //Rewards
         if (AbstractDungeon.ascensionLevel >= 15) {//Asc 15
 
-            //First impactful choice
-            if (AbstractDungeon.miscRng.randomBoolean()) {
-                this.imageEventText.setDialogOption(OPTIONS[0] + 20 + OPTIONS[1]);
+            //First impactful reward
+            if (T_DEADBRANCH_F_FLINT) {
+                curse.upgrade();
             } else {
-                this.imageEventText.setDialogOption(OPTIONS[2] + FLINT_HP_LOSS + OPTIONS[3]);
+                hpLoss = (int)(AbstractDungeon.player.maxHealth * A_15_FLINT_HP_LOSS);
             }
 
-            //Second light choice
-            if (AbstractDungeon.miscRng.randomBoolean()) {
-                this.imageEventText.setDialogOption(OPTIONS[4] + GOLD + OPTIONS[5]);
+            //Second light reward
+            if (T_GOLD_F_HEALING) {
+                goldGain = A_15_GOLD;
             } else {
-                this.imageEventText.setDialogOption(OPTIONS[6] + HEALING + OPTIONS[7]);
+                healing = A_15_HEALING;
             }
 
         } else {//Non-Asc 15
 
-            //First impactful choice
-            if (AbstractDungeon.miscRng.randomBoolean()) {
-                this.imageEventText.setDialogOption(OPTIONS[0] + 20 + OPTIONS[1]);
-            } else {
-                this.imageEventText.setDialogOption(OPTIONS[2] + A_15_FLINT_HP_LOSS + OPTIONS[3]);
+            //First impactful reward
+            if (!T_DEADBRANCH_F_FLINT) {
+                hpLoss = (int)(AbstractDungeon.player.maxHealth * FLINT_HP_LOSS);
             }
 
-            //Second light choice
-            if (AbstractDungeon.miscRng.randomBoolean()) {
-                this.imageEventText.setDialogOption(OPTIONS[4] + A_15_GOLD + OPTIONS[5]);
+            //Second light reward
+            if (T_GOLD_F_HEALING) {
+                goldGain = GOLD;
             } else {
-                this.imageEventText.setDialogOption(OPTIONS[6] + A_15_HEALING + OPTIONS[7]);
+                healing = HEALING;
             }
 
         }
+
+        //Text
+        //First impactful choice
+        if (T_DEADBRANCH_F_FLINT) {
+            this.imageEventText.setDialogOption(OPTIONS[0]);
+        } else {
+            this.imageEventText.setDialogOption(OPTIONS[1] + hpLoss + OPTIONS[2]);
+        }
+
+        //Second light choice
+        if (T_GOLD_F_HEALING) {
+            this.imageEventText.setDialogOption(OPTIONS[3] + goldGain + OPTIONS[4]);
+        } else {
+            this.imageEventText.setDialogOption(OPTIONS[5] + healing + OPTIONS[6]);
+        }
+
+        this.imageEventText.setDialogOption(OPTIONS[7]);
     }
 
-    @Override
     protected void buttonEffect(int buttonPressed) {
-        switch(this.screen) {
-            case INTRO:
-                this.imageEventText.updateBodyText(DESCRIPTIONS[1]);
-                this.imageEventText.updateDialogOption(0, OPTIONS[1]);
-                this.screen = CurScreen.FIGHT;
-                break;
-            case FIGHT:
-                this.screen = CurScreen.POST_COMBAT;
-                logMetric(ID,"Fight");
-                AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter(ThievesHideoutZone.BANDIT_LIEUTENANT_AND_TASKMASTER);
-                AbstractDungeon.getCurrRoom().rewards.clear();
-                AbstractDungeon.getCurrRoom().rewardAllowed = false;
-                this.enterCombatFromImage();
-                AbstractDungeon.lastCombatMetricKey = ThievesHideoutZone.BANDIT_LIEUTENANT_AND_TASKMASTER;
-                break;
-            case POST_COMBAT:
-                AbstractDungeon.getCurrRoom().rewardAllowed = true;
-                switch(buttonPressed) {
-                    case 1:
-                        logMetric(ID,"Greed");
-                        AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter(ThievesHideoutZone.THIEF_KING);
-                        AbstractDungeon.getCurrRoom().rewards.clear();
-                        AbstractDungeon.getCurrRoom().addRelicToRewards(RelicTier.RARE);
-                        AbstractDungeon.getCurrRoom().addRelicToRewards(RelicTier.UNCOMMON);
-                        AbstractDungeon.getCurrRoom().addGoldToRewards(GOLD);
-                        AbstractDungeon.getCurrRoom().eliteTrigger = true;
-                        this.enterCombatFromImage();
-                        AbstractDungeon.lastCombatMetricKey = ThievesHideoutZone.THIEF_KING;
-                        break;
-                    default:
-                        this.screen = CurScreen.LEAVE;
-                        AbstractDungeon.effectList.add(new RainingGoldEffect(GOLD));
-                        AbstractDungeon.player.gainGold(GOLD);
-                        logMetricGainGold(ID, "Prudence", GOLD);
-                        this.imageEventText.updateBodyText(DESCRIPTIONS[3]);
-                        this.imageEventText.clearAllDialogs();
-                        this.imageEventText.setDialogOption(OPTIONS[4]);
-                        break;
+        switch (buttonPressed) {
+            case 0:
+                if (T_DEADBRANCH_F_FLINT)
+                    AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(curse, (float)Settings.WIDTH / 2.0F, (float)Settings.HEIGHT / 2.0F));
+                else {
+                    AbstractDungeon.player.damage(new DamageInfo((AbstractCreature) null, hpLoss));
                 }
+
+                AbstractDungeon.getCurrRoom().rewards.clear();
+                String targetRelicId = T_DEADBRANCH_F_FLINT ? DeadBranch.ID : Circlet.ID;
+                AbstractRelic relic = RelicLibrary.getRelic(targetRelicId).makeCopy();
+                AbstractDungeon.getCurrRoom().rewards.add(new RewardItem(relic));
+
+                AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
+                AbstractDungeon.combatRewardScreen.open();
                 break;
-            default:
-                this.openMap();
+            case 1:
+                if (T_GOLD_F_HEALING)
+                    AbstractDungeon.player.gainGold(goldGain);
+                else {
+                    AbstractDungeon.player.heal(healing, true);
+                }
+
+                AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
+                break;
+            case 2:
+                AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
                 break;
         }
     }
 
-    @Override
-    public void reopen() {
-        if (this.screen != CurScreen.LEAVE) {
-            AbstractDungeon.resetPlayer();
-            AbstractDungeon.player.drawX = (float) Settings.WIDTH * 0.25F;
-            AbstractDungeon.player.preBattlePrep();
-            this.enterImageFromCombat();
-            this.imageEventText.updateBodyText(DESCRIPTIONS[2]);
-            this.imageEventText.updateDialogOption(0, OPTIONS[2].replace("{0}", GOLD + ""));
-            this.imageEventText.setDialogOption(OPTIONS[3]);
-        }
-    }
 
-    private enum CurScreen {
-        INTRO,
-        FIGHT,
-        LEAVE,
-        POST_COMBAT
-    }
 }
---
+++ src/main/java/spireMapOverhaul/zones/hailstorm/relics/Flint.java
@@ -0,0 +1,28 @@
+package spireMapOverhaul.zones.hailstorm.relics;
+
+import basemod.AutoAdd;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.relics.Damaru;
+import spireMapOverhaul.zones.brokenSpace.relics.BrokenRelic;
+
+import static spireMapOverhaul.util.Wiz.adp;
+
+@AutoAdd.Ignore
+public class Flint extends BrokenRelic {
+    public static final String ID = Flint.class.getSimpleName();
+
+    public Flint() {
+        super(ID, RelicTier.SPECIAL, LandingSound.CLINK, Flint.ID);
+    }
+
+    @Override
+    public boolean canSpawn() {
+        return adp().chosenClass == AbstractPlayer.PlayerClass.WATCHER;
+    }
+
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+}
