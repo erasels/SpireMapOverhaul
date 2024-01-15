aae8e1c6d6e69bcfaf27a045e7723cd27f6c759a
ActuallyEnbeon <85409980+ActuallyEnbeon@users.noreply.github.com>
2024-01-15 15:05:27 +0000

    Changed (nerfed) Stone Fragment

--- src/main/java/spireMapOverhaul/zones/thefog/relics/StoneFragment.java
+++ src/main/java/spireMapOverhaul/zones/thefog/relics/StoneFragment.java
@@ -1,7 +1,9 @@
 package spireMapOverhaul.zones.thefog.relics;
 
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PowerTip;
 import spireMapOverhaul.abstracts.AbstractSMORelic;
 import spireMapOverhaul.zones.thefog.TheFogZone;
 
@@ -13,30 +15,33 @@ public class StoneFragment extends AbstractSMORelic {
 
     public StoneFragment() {
         super(ID, TheFogZone.ID, RelicTier.SPECIAL, LandingSound.HEAVY);
+        counter = 13;
     }
 
     @Override
     public void atTurnStart() {
-        pulse = true;
-        beginPulse();
-        counter = 0;
-    }
-
-    @Override
-    public void onCardDraw(AbstractCard drawnCard) {
-        counter += 1;
-        if (counter == 6) {
-            flash();
-            pulse = false;
-            stopPulse();
-            atb(new GainEnergyAction(1));
+        if (counter < 1) return;
+        flash();
+        atb(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        atb(new GainEnergyAction(1));
+        counter--;
+        if (counter < 1) {
+            counter = -1;
+            usedUp();
         }
     }
 
     @Override
-    public void onVictory() {
-        pulse = false;
-        stopPulse();
-        counter = -1;
+    public void update() {
+        super.update();
+        description = (counter < 1) ? MSG[2] : DESCRIPTIONS[1]; // MSG[2] is the "used up" string
+        tips.clear();
+        tips.add(new PowerTip(name, description));
+        initializeTips();
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0] + DESCRIPTIONS[1];
     }
 }
