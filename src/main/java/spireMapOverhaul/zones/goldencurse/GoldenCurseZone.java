4ba628204c48ac52cf0f0afb49880b91beef0b9f
tetraminus <54343976+tetraminus@users.noreply.github.com>
2024-01-15 18:51:22 -0800

    account for other types of rewards

--- src/main/java/spireMapOverhaul/zones/goldencurse/GoldenCurseZone.java
+++ src/main/java/spireMapOverhaul/zones/goldencurse/GoldenCurseZone.java
@@ -1,17 +1,24 @@
 package spireMapOverhaul.zones.goldencurse;
 
 import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.rewards.RewardItem;
 import spireMapOverhaul.abstracts.AbstractZone;
+import spireMapOverhaul.patches.CustomRewardTypes;
+import spireMapOverhaul.rewards.SingleCardReward;
 import spireMapOverhaul.zoneInterfaces.RewardModifyingZone;
 
 import java.util.ArrayList;
 
+import static spireMapOverhaul.patches.CustomRewardTypes.SMO_SINGLECARDREWARD;
+
 public class GoldenCurseZone extends AbstractZone implements RewardModifyingZone {
     public static final String ID = "GoldenCurse";
 
+    public static final int BASE_CARD_COST = 30;
+    public static final int BASE_HEAL_COST = 10;
 
     private final Color color = Color.GOLD.cpy();
 
@@ -19,8 +26,8 @@ public class GoldenCurseZone extends AbstractZone implements RewardModifyingZone
         super(ID);
         this.width = 2;
         this.maxWidth = 3;
-        this.height = 3;
-        this.maxHeight = 4;
+        this.height = 2;
+        this.maxHeight = 3;
         iconTexture = ImageMaster.MAP_NODE_ELITE;
 
     }
@@ -29,7 +36,7 @@ public class GoldenCurseZone extends AbstractZone implements RewardModifyingZone
 
         switch (instance.type) {
             case CARD:
-                return 50;
+                return BASE_CARD_COST;
             case RELIC:
                 return instance.relic.getPrice() / 2;
             case POTION:
@@ -38,16 +45,30 @@ public class GoldenCurseZone extends AbstractZone implements RewardModifyingZone
             case SAPPHIRE_KEY:
             case EMERALD_KEY:
                 return 0;
+            default:
+                break;
         }
 
+        if (instance.type == SMO_SINGLECARDREWARD && instance instanceof SingleCardReward) {
+            return AbstractCard.getPrice(((SingleCardReward) instance).card.rarity) / 2;
+        }
 
-        return 50;
+        if (instance.type == CustomRewardTypes.SMO_ANYCOLORCARDREWARD) {
+            return BASE_CARD_COST;
+        }
+
+        if (instance.type == CustomRewardTypes.HEALREWARD) {
+            return BASE_HEAL_COST;
+        }
+
+        return 30;
     }
 
     @Override
     public ArrayList<RewardItem> getAdditionalRewards() {
         ArrayList<RewardItem> rewards = new ArrayList<>();
         rewards.add(new RewardItem());
+        rewards.add(new RewardItem(AbstractDungeon.returnRandomPotion()));
         if (AbstractDungeon.getCurrRoom().rewards.stream().anyMatch(r -> r.type == RewardItem.RewardType.RELIC)) {
             rewards.add(new RewardItem(AbstractDungeon.returnRandomRelic(AbstractDungeon.returnRandomRelicTier())));
         }
