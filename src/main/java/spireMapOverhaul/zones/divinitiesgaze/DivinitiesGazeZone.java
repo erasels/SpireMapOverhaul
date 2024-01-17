18971e57db910de0f3f8c1fc72a810b88806840b
NellyDevo <Noelle.Devonshire@yahoo.com>
2024-01-16 19:08:19 -0800

    Add boon and statuses and speech bubble at battle start

--- src/main/java/spireMapOverhaul/zones/divinitiesgaze/DivinitiesGazeZone.java
+++ src/main/java/spireMapOverhaul/zones/divinitiesgaze/DivinitiesGazeZone.java
@@ -3,14 +3,23 @@ package spireMapOverhaul.zones.divinitiesgaze;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+import com.megacrit.cardcrawl.vfx.SpeechBubble;
 import spireMapOverhaul.abstracts.AbstractZone;
 import spireMapOverhaul.util.Wiz;
 import spireMapOverhaul.zoneInterfaces.CombatModifyingZone;
 import spireMapOverhaul.zoneInterfaces.ModifiedEventRateZone;
+import spireMapOverhaul.zones.divinitiesgaze.divinities.DivineBeing;
+import spireMapOverhaul.zones.divinitiesgaze.divinities.DivineBeingManager;
 import spireMapOverhaul.zones.divinitiesgaze.divinities.impl.Jurors;
 import spireMapOverhaul.zones.divinitiesgaze.events.DivineVisitor;
 import spireMapOverhaul.zones.divinitiesgaze.powers.VerdictPower;
+
+import java.util.Random;
 
 public class DivinitiesGazeZone extends AbstractZone implements ModifiedEventRateZone, CombatModifyingZone {
 
@@ -39,17 +48,29 @@ public class DivinitiesGazeZone extends AbstractZone implements ModifiedEventRat
 
   @Override
   public void atPreBattle() {
-    if (!Jurors.doApplyVerdictOnCombatStart) {
-      return;
+    if (Jurors.doApplyVerdictOnCombatStart) {
+        Jurors.doApplyVerdictOnCombatStart = false;
+        Wiz.atb(new AbstractGameAction() {
+            @Override
+            public void update() {
+                AbstractDungeon.getMonsters().monsters.forEach(m -> Wiz.att(new ApplyPowerAction(m, AbstractDungeon.player, new VerdictPower(m,  10), 10)));
+                this.isDone = true;
+            }
+        });
     }
 
-    Jurors.doApplyVerdictOnCombatStart = false;
+    DivineBeing being = DivineBeingManager.getDivinityForCombat();
+    AbstractCard boon = CardLibrary.getCard(being.getDivinityStrings().getBoonCardId());
+    AbstractCard status = CardLibrary.getCard(being.getDivinityStrings().getStatusCardId());
+    Wiz.atb(new MakeTempCardInDrawPileAction(boon, 1, true, true));
+    Wiz.atb(new MakeTempCardInDrawPileAction(status, AbstractDungeon.actNum, true, true));
+    String[] quotes = being.getDivinityStrings().getPreCombatQuotes();
     Wiz.atb(new AbstractGameAction() {
-      @Override
-      public void update() {
-        AbstractDungeon.getMonsters().monsters.forEach(m -> Wiz.att(new ApplyPowerAction(m, AbstractDungeon.player, new VerdictPower(m,  10), 10)));
-        this.isDone = true;
-      }
+        @Override
+        public void update() {
+            AbstractDungeon.effectList.add(new SpeechBubble(Settings.WIDTH / 2f, Settings.HEIGHT * 3f / 4f, 3, quotes[new Random().nextInt(quotes.length)], false));
+            isDone = true;
+        }
     });
   }
 }
