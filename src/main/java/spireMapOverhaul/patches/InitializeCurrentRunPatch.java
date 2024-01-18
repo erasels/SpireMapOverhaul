4e83ccaa954d13274fbe634b1d5c6af0748310c0
modargo <modargo@gmail.com>
2024-01-18 01:06:07 -0500

    Disable zones in Downfall mode, since there are currently issues with the flipped map. This is likely something we can fix and support, but Downfall support doesn't need to be in the initial release

--- src/main/java/spireMapOverhaul/patches/InitializeCurrentRunPatch.java
+++ src/main/java/spireMapOverhaul/patches/InitializeCurrentRunPatch.java
@@ -5,6 +5,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import spireMapOverhaul.SpireAnniversary6Mod;
+import spireMapOverhaul.util.DownfallUtil;
 
 import java.util.HashSet;
 import java.util.stream.Collectors;
@@ -13,7 +14,7 @@ import java.util.stream.Collectors;
 public class InitializeCurrentRunPatch {
     @SpirePostfixPatch
     public static void initializeCurrentRun() {
-        SpireAnniversary6Mod.currentRunActive = SpireAnniversary6Mod.getActiveConfig();
+        SpireAnniversary6Mod.currentRunActive = SpireAnniversary6Mod.getActiveConfig() && !DownfallUtil.isDownfallMode();
         SpireAnniversary6Mod.currentRunNoRepeatZones = SpireAnniversary6Mod.getNoRepeatZonesConfig();
         SpireAnniversary6Mod.currentRunAllZones = SpireAnniversary6Mod.unfilteredAllZones.stream()
                 .map(z -> z.id)
