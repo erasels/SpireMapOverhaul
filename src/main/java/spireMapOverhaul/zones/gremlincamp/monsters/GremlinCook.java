c9cf79e1e65c623090f5ffcbfecc38f1065025be
erasels <emanuelbedburdick@yahoo.de>
2024-01-19 21:22:58 +0100

    Add a cauldron and effects to Gremlin cook

--- src/main/java/spireMapOverhaul/zones/gremlincamp/monsters/GremlinCook.java
+++ src/main/java/spireMapOverhaul/zones/gremlincamp/monsters/GremlinCook.java
@@ -1,5 +1,11 @@
 package spireMapOverhaul.zones.gremlincamp.monsters;
 
+import basemod.ReflectionHacks;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.evacipated.cardcrawl.mod.stslib.actions.common.AllEnemyApplyPowerAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.GameActionManager;
@@ -12,21 +18,31 @@ import com.megacrit.cardcrawl.cards.status.Dazed;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.MonsterStrings;
 import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.vfx.ExhaustEmberEffect;
+import com.megacrit.cardcrawl.vfx.combat.FlameParticleEffect;
 import com.megacrit.cardcrawl.vfx.combat.HealEffect;
+import com.megacrit.cardcrawl.vfx.combat.SmokeBlurEffect;
 import spireMapOverhaul.SpireAnniversary6Mod;
 import spireMapOverhaul.abstracts.AbstractSMOMonster;
+import spireMapOverhaul.util.TexLoader;
 import spireMapOverhaul.util.Wiz;
+import spireMapOverhaul.zones.gremlincamp.EmberEffect;
+import spireMapOverhaul.zones.gremlincamp.GremlinCamp;
 import spireMapOverhaul.zones.gremlincamp.PlayerPoisonPower;
+import spireMapOverhaul.zones.gremlincamp.SmokeEffect;
 
 import java.util.ArrayList;
 
 import static spireMapOverhaul.SpireAnniversary6Mod.makeID;
+import static spireMapOverhaul.SpireAnniversary6Mod.makeMonsterPath;
 
 public class GremlinCook extends AbstractSMOMonster {
     public static final String ID = makeID(GremlinCook.class.getSimpleName());
     private static final MonsterStrings monsterStrings = CardCrawlGame.languagePack.getMonsterStrings(ID);
+    private static final Texture cauldron = TexLoader.getTexture(makeMonsterPath(GremlinCamp.ID+"/cauldron.png"));
     public static final String NAME = monsterStrings.NAME;
     public static final String[] MOVES = monsterStrings.MOVES;
 
@@ -34,6 +50,7 @@ public class GremlinCook extends AbstractSMOMonster {
 
     private int healAmt, strAmt, poisonAmt;
     private boolean secondMove = false;
+    private float cauldron_x;
 
     public GremlinCook() {
         this(0, 0);
@@ -51,6 +68,8 @@ public class GremlinCook extends AbstractSMOMonster {
         healAmt = calcAscensionSpecial(10);
         strAmt = AbstractDungeon.ascensionLevel >= 18? 3: 2;
         poisonAmt = calcAscensionSpecial(6);
+
+        cauldron_x = hb.x;
     }
 
     @Override
@@ -133,5 +152,35 @@ public class GremlinCook extends AbstractSMOMonster {
 
         byte move = Wiz.getRandomItem(possibilities, AbstractDungeon.aiRng);
         setMoveShortcut(move, MOVES[move]);
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        super.render(sb);
+        //This will always render, even if dead or escaped
+        sb.setColor(Color.WHITE);
+        float w = cauldron.getWidth() * Settings.scale, h = cauldron.getHeight() * Settings.scale;
+        sb.draw(cauldron, cauldron_x - w, hb.y, w, h);
+    }
+
+    private static final float CEMBER_MAX = 0.5f, CSMOKE_MAX = 0.7f;
+    private float cEmber = 1f, cSmoke = 1;
+
+    @Override
+    public void update() {
+        super.update();
+        cEmber -= Gdx.graphics.getRawDeltaTime();
+        cSmoke -= Gdx.graphics.getRawDeltaTime();
+
+        if(cEmber <= 0) {
+            cEmber = CEMBER_MAX * MathUtils.random(0.5f, 1);
+            EmberEffect eff = new EmberEffect(MathUtils.random(cauldron_x - cauldron.getWidth() * Settings.scale, cauldron_x), hb.y + 10f * Settings.scale);
+            AbstractDungeon.effectList.add(eff);
+        }
+        if(cSmoke <= 0) {
+            cSmoke = CSMOKE_MAX * MathUtils.random(0.5f, 1);
+            SmokeEffect eff = new SmokeEffect(MathUtils.random(cauldron_x - cauldron.getWidth() * Settings.scale, cauldron_x), hb.y + ((cauldron.getHeight() - 20) * Settings.scale));
+            AbstractDungeon.effectList.add(eff);
+        }
     }
 }
