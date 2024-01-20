441e6237ef5167b45755fe6c723e7d4883dcb2c2
shammysham <139022155+shammysham@users.noreply.github.com>
2024-01-20 10:51:12 -0500

    Add tooltip for Verdict on Jurors event, update Mana Cyclone event to use new helper method. Reduce Eclectic card choices from 20 to 10.

--- src/main/java/spireMapOverhaul/zones/divinitiesgaze/divinities/DivineBeing.java
+++ src/main/java/spireMapOverhaul/zones/divinitiesgaze/divinities/DivineBeing.java
@@ -8,17 +8,23 @@ import java.util.function.Supplier;
 public interface DivineBeing {
   DivinityStrings getDivinityStrings();
   Consumer<Integer> doEventButtonAction();
+
   default String getCombatPhaseKey() {
     return "";
   }
+
   default Supplier<Boolean> isEventOptionEnabled() {
     return () -> true;
   }
+
   default Consumer<AbstractRoom> getCombatRewards() { return room -> {};}
+
   default boolean doUpdate() {
     return true;
   }
+
   default void doEnterCombat() {}
+
   default boolean hasUpdateLogic() {
     return false;
   }
@@ -37,5 +43,13 @@ public interface DivineBeing {
 
   default String getEventButtonUnavailableText() {
     return getDivinityStrings().getEventButtonUnavailableText();
+  }
+
+  default String[] getKeywordsForCardChoice(){
+    return new String[]{};
+  }
+
+  default String[] getKeywordsForCustomChoice(){
+    return new String[]{};
   }
 }
