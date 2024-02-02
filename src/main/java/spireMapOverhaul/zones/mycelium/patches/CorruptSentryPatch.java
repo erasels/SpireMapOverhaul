c21537c25365de87bea0e5eaefe271645b052c20
lobbienjonsji <36815592+lobbienjonsji@users.noreply.github.com>
2024-02-02 21:10:38 +0100

    Changed patch to ONLY affect the Automata fight

--- src/main/java/spireMapOverhaul/zones/mycelium/patches/CorruptSentryPatch.java
+++ src/main/java/spireMapOverhaul/zones/mycelium/patches/CorruptSentryPatch.java
@@ -2,29 +2,28 @@ package spireMapOverhaul.zones.mycelium.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpireInstrumentPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.animations.TalkAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.MonsterStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.exordium.Sentry;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.ArtifactPower;
 import com.megacrit.cardcrawl.powers.PoisonPower;
 import com.megacrit.cardcrawl.powers.SporeCloudPower;
 import javassist.CannotCompileException;
-import javassist.expr.ConstructorCall;
 import javassist.expr.ExprEditor;
-import javassist.expr.MethodCall;
 import javassist.expr.NewExpr;
 import spireMapOverhaul.SpireAnniversary6Mod;
-import spireMapOverhaul.abstracts.AbstractZone;
 import spireMapOverhaul.util.Wiz;
-import spireMapOverhaul.zones.mycelium.Mycelium;
+import spireMapOverhaul.zones.mycelium.monsters.FungalFriend;
 
-import java.util.Objects;
+
+import static java.lang.System.in;
+import static org.lwjgl.util.mapped.MappedObject.foreach;
 
 @SpirePatch(
         clz = Sentry.class,
@@ -54,14 +53,15 @@ public class CorruptSentryPatch {
         };
     }
     
-    public static AbstractPower CorruptSentry(AbstractCreature sentry, int amount)
-    {
-        AbstractZone curZone = Wiz.getCurZone();
-        if(Wiz.getCurZone() != null && Wiz.getCurZone() instanceof Mycelium) {
-            sentry.name = values.NAME;
-            AbstractDungeon.actionManager.addToBottom(new TalkAction(sentry, values.DIALOG[0], 0.5F, 2.0F));
-            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(sentry, sentry, new SporeCloudPower(sentry, 2)));
-            return new PoisonPower(sentry, sentry, 6);
+    public static AbstractPower CorruptSentry(AbstractCreature sentry, int amount) {
+        for (AbstractMonster m : Wiz.getEnemies())
+        {
+            if (m instanceof FungalFriend) {
+                sentry.name = values.NAME;
+                AbstractDungeon.actionManager.addToBottom(new TalkAction(sentry, values.DIALOG[0], 0.5F, 2.0F));
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(sentry, sentry, new SporeCloudPower(sentry, 2)));
+                return new PoisonPower(sentry, sentry, 6);
+            }
         }
         return new ArtifactPower(sentry, amount);
     }
