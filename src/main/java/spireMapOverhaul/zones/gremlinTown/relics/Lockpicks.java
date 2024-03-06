da81a20babcad075890a7740d7b069df23f20200
modargo <modargo@gmail.com>
2024-03-06 16:31:40 -0500

    Relic stats: Lockpicks

--- src/main/java/spireMapOverhaul/zones/gremlinTown/relics/Lockpicks.java
+++ src/main/java/spireMapOverhaul/zones/gremlinTown/relics/Lockpicks.java
@@ -1,9 +1,17 @@
 package spireMapOverhaul.zones.gremlinTown.relics;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.rooms.RestRoom;
 import spireMapOverhaul.abstracts.AbstractSMORelic;
 import spireMapOverhaul.zones.gremlinTown.GremlinTown;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static spireMapOverhaul.SpireAnniversary6Mod.makeID;
 import static spireMapOverhaul.util.Wiz.adp;
@@ -26,6 +34,42 @@ public class Lockpicks extends AbstractSMORelic {
         if (room instanceof RestRoom) {
             flash();
             adp().gainGold(GOLD_AMOUNT);
+            incrementGoldStat(GOLD_AMOUNT);
         }
+    }
+
+    private static final Map<String, Integer> stats = new HashMap<>();
+    private static final String GOLD_STAT = "gold";
+
+    public String getStatsDescription() {
+        return DESCRIPTIONS[3].replace("{0}", stats.get(GOLD_STAT) + "");
+    }
+
+    public String getExtendedStatsDescription(int totalCombats, int totalTurns) {
+        return getStatsDescription();
+    }
+
+    public void resetStats() {
+        stats.put(GOLD_STAT, 0);
+    }
+
+    public JsonElement onSaveStats() {
+        Gson gson = new Gson();
+        List<Integer> statsToSave = new ArrayList<>();
+        statsToSave.add(stats.get(GOLD_STAT));
+        return gson.toJsonTree(statsToSave);
+    }
+
+    public void onLoadStats(JsonElement jsonElement) {
+        if (jsonElement != null) {
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            stats.put(GOLD_STAT, jsonArray.get(0).getAsInt());
+        } else {
+            resetStats();
+        }
+    }
+
+    public static void incrementGoldStat(int amount) {
+        stats.put(GOLD_STAT, stats.getOrDefault(GOLD_STAT, 0) + amount);
     }
 }
