a1bab428b1ad57bf71d7098928367032acc24699
modargo <modargo@gmail.com>
2024-03-06 08:27:19 -0500

    Relic stats: Broken Snecko Skull

--- src/main/java/spireMapOverhaul/zones/brokenspace/relics/BrokenSneckoSkull.java
+++ src/main/java/spireMapOverhaul/zones/brokenspace/relics/BrokenSneckoSkull.java
@@ -1,10 +1,19 @@
 package spireMapOverhaul.zones.brokenspace.relics;
 
 import com.evacipated.cardcrawl.mod.stslib.relics.OnReceivePowerRelic;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.relics.SneckoSkull;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static spireMapOverhaul.util.Wiz.adp;
 
@@ -36,6 +45,7 @@ public class BrokenSneckoSkull extends BrokenRelic implements OnReceivePowerReli
     public int onReceivePowerStacks(AbstractPower power, AbstractCreature source, int stackAmount) {
         flash();
         if (power.type == AbstractPower.PowerType.BUFF) {
+            incrementBuffsStat(AMOUNT);
             return stackAmount + AMOUNT;
         }
         return stackAmount;
@@ -44,5 +54,43 @@ public class BrokenSneckoSkull extends BrokenRelic implements OnReceivePowerReli
     @Override
     public boolean canSpawn() {
         return adp().chosenClass == AbstractPlayer.PlayerClass.THE_SILENT;
+    }
+
+    private static final Map<String, Integer> stats = new HashMap<>();
+    private static final String BUFFS_STAT = "buffs";
+
+    public String getStatsDescription() {
+        return DESCRIPTIONS[2].replace("{0}", stats.get(BUFFS_STAT) + "");
+    }
+
+    public String getExtendedStatsDescription(int totalCombats, int totalTurns) {
+        DecimalFormat format = new DecimalFormat("#.###");
+        float buffs = stats.get(BUFFS_STAT);
+        String buffsPerCombat = format.format(buffs / Math.max(totalCombats, 1));
+        return getStatsDescription() + DESCRIPTIONS[3].replace("{0}", buffsPerCombat);
+    }
+
+    public void resetStats() {
+        stats.put(BUFFS_STAT, 0);
+    }
+
+    public JsonElement onSaveStats() {
+        Gson gson = new Gson();
+        List<Integer> statsToSave = new ArrayList<>();
+        statsToSave.add(stats.get(BUFFS_STAT));
+        return gson.toJsonTree(statsToSave);
+    }
+
+    public void onLoadStats(JsonElement jsonElement) {
+        if (jsonElement != null) {
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            stats.put(BUFFS_STAT, jsonArray.get(0).getAsInt());
+        } else {
+            resetStats();
+        }
+    }
+
+    public static void incrementBuffsStat(int amount) {
+        stats.put(BUFFS_STAT, stats.getOrDefault(BUFFS_STAT, 0) + amount);
     }
 }
