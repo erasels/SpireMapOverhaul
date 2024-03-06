f29b1a0085b99e7703b04dd78d520d6aadb507f3
modargo <modargo@gmail.com>
2024-03-06 16:13:44 -0500

    Relic stats: Nob Club

--- src/main/java/spireMapOverhaul/zones/gremlinTown/relics/NobClub.java
+++ src/main/java/spireMapOverhaul/zones/gremlinTown/relics/NobClub.java
@@ -1,5 +1,8 @@
 package spireMapOverhaul.zones.gremlinTown.relics;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -8,6 +11,13 @@ import com.megacrit.cardcrawl.powers.VulnerablePower;
 import spireMapOverhaul.abstracts.AbstractSMORelic;
 import spireMapOverhaul.util.Wiz;
 import spireMapOverhaul.zones.gremlinTown.GremlinTown;
+
+import java.text.DecimalFormat;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static spireMapOverhaul.SpireAnniversary6Mod.makeID;
 import static spireMapOverhaul.util.Wiz.adp;
@@ -38,7 +48,47 @@ public class NobClub extends AbstractSMORelic {
                 addToBot(new RelicAboveCreatureAction(adp(), this));
                 AbstractMonster m = Wiz.getRandomEnemy();
                 applyToEnemy(m, new VulnerablePower(m, 1, false));
+                incrementVulnerableStat(1);
             }
         }
+    }
+
+    private static final Map<String, Integer> stats = new HashMap<>();
+    private static final String VULNERABLE_STAT = "vulnerable";
+
+    public String getStatsDescription() {
+        return MessageFormat.format(DESCRIPTIONS[1], stats.get(VULNERABLE_STAT));
+    }
+
+    public String getExtendedStatsDescription(int totalCombats, int totalTurns) {
+        DecimalFormat format = new DecimalFormat("#.###");
+        float vulnerable = stats.get(VULNERABLE_STAT);
+        String vulnerablePerTurn = format.format(vulnerable / Math.max(totalTurns, 1));
+        String vulnerablePerCombat = format.format(vulnerable / Math.max(totalCombats, 1));
+        return getStatsDescription() + MessageFormat.format(DESCRIPTIONS[2], vulnerablePerTurn, vulnerablePerCombat);
+    }
+
+    public void resetStats() {
+        stats.put(VULNERABLE_STAT, 0);
+    }
+
+    public JsonElement onSaveStats() {
+        Gson gson = new Gson();
+        List<Integer> statsToSave = new ArrayList<>();
+        statsToSave.add(stats.get(VULNERABLE_STAT));
+        return gson.toJsonTree(statsToSave);
+    }
+
+    public void onLoadStats(JsonElement jsonElement) {
+        if (jsonElement != null) {
+            JsonArray jsonArray = jsonElement.getAsJsonArray();
+            stats.put(VULNERABLE_STAT, jsonArray.get(0).getAsInt());
+        } else {
+            resetStats();
+        }
+    }
+
+    public static void incrementVulnerableStat(int amount) {
+        stats.put(VULNERABLE_STAT, stats.getOrDefault(VULNERABLE_STAT, 0) + amount);
     }
 }
