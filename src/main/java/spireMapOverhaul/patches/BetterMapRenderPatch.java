a904c9a75941b5e7ef31c522b09decb6b952b64a
modargo <modargo@gmail.com>
2024-04-15 10:37:32 -0400

    Adjust zone outline compatbility logic for Dark Map to avoid potential issues with other combinations of mods that affect map display

--- src/main/java/spireMapOverhaul/patches/BetterMapRenderPatch.java
+++ src/main/java/spireMapOverhaul/patches/BetterMapRenderPatch.java
@@ -2,6 +2,7 @@ package spireMapOverhaul.patches;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.modthespire.Loader;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -9,13 +10,34 @@ import com.megacrit.cardcrawl.map.DungeonMap;
 import spireMapOverhaul.BetterMapGenerator;
 import spireMapOverhaul.abstracts.AbstractZone;
 
-@SpirePatch(
-        clz = DungeonMap.class,
-        method = "renderNormalMap"
-)
 public class BetterMapRenderPatch {
-    @SpirePostfixPatch
-    public static void render(DungeonMap __instance, SpriteBatch sb) {
+    @SpirePatch(
+            clz = DungeonMap.class,
+            method = "renderMapBlender"
+    )
+    public static class NormalMapPatch {
+        @SpirePostfixPatch
+        public static void render(DungeonMap __instance, SpriteBatch sb) {
+            if (!Loader.isModLoaded("ojb_DarkMap")) {
+                renderZones(sb);
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = DungeonMap.class,
+            method = "renderNormalMap"
+    )
+    public static class DarkMapPatch {
+        @SpirePostfixPatch
+        public static void render(DungeonMap __instance, SpriteBatch sb) {
+            if (Loader.isModLoaded("ojb_DarkMap")) {
+                renderZones(sb);
+            }
+        }
+    }
+
+    private static void renderZones(SpriteBatch sb) {
         Color old = sb.getColor();
 
         for (AbstractZone zone : BetterMapGenerator.getActiveZones(AbstractDungeon.map)) {
